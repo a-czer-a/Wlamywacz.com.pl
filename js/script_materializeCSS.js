9acd37c3dbab7a0cb84c360ad5bd5a6e69378642
Agata Czerwińska <agata@rubrica.pl>
2018-10-13 20:25:30 +0200

    Change hero img

--- js/script_materializeCSS.js
+++ js/script_materializeCSS.js
@@ -1,9 +1,4 @@
 $(document).ready(function () {
     $(".button-collapse").sideNav();
-//    $(".parallax").parallax();
-//    $('.slider').slider({
-//        indicators: false,
-//        height: 500
-//    });
 });
 
